a3e668a70aed40ba1e89023cb14247c23a55009c
franciscocamellon <franciscocamellon@gmail.com>
2022-11-03 16:22:24 -0300

    Impressão

--- testedeperformance01/PrimeiroTP.java
+++ testedeperformance01/PrimeiroTP.java
@@ -7,6 +7,26 @@ public class PrimeiroTP {
 
 	private static final int LIMIT = 100; 	
 
+    private static void printReport() {		
+		for (int i = 0; i < LIMIT; i++) {
+			if(studentNames[i] != null) {
+				printReport(i);
+			}
+		}
+	}
+
+	private static void printReport(int indice) {
+		float averageGrade = calculateAverageGrade(indice);
+		
+		System.out.println("<"+indice+"> " + 
+							studentNames[indice] + " -- " + 
+							"AV1: " + firstGrade[indice] + " -- " + 
+							"AV2: " + secondGrade[indice] + " -- " + 
+							"Média: " + averageGrade + " -- " +
+							"Situação: " + getStudentStatus(averageGrade)
+							);
+	}
+
     private static float calculateAverageGrade(int indice){
 		return (firstGrade[indice] + secondGrade[indice]) / 2;
 	}
