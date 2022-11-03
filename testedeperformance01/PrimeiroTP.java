44bfdfa063dc013918a7876a39565b6c50057399
franciscocamellon <franciscocamellon@gmail.com>
2022-11-03 16:39:43 -0300

    Menu escolha

--- testedeperformance01/PrimeiroTP.java
+++ testedeperformance01/PrimeiroTP.java
@@ -50,6 +50,22 @@ public class PrimeiroTP {
 
         String userOption;
 		int indexPosition = 0;
+
+        do {
+			System.out.println("[1] Registrar as notas de um novo aluno.");
+			System.out.println("[2] Consultar boletim de um aluno.");
+			System.out.println("[3] Consultar notas da turma.");
+			System.out.println("[4] Sair.");
+			
+			System.out.print("Informe a opção desejada: ");						
+			userOption = in.next();
+			
+			
+		} while (!"4".equalsIgnoreCase(userOption));
+				
+		System.out.println("Processamento finalizado!!!");
+		
+		in.close();
 		
 	}
 }
