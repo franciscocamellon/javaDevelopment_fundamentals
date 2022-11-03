673664f53383c5f7d6fe3709898980218c44c3c6
franciscocamellon <franciscocamellon@gmail.com>
2022-11-03 16:17:28 -0300

    Regra de negócio

--- testedeperformance01/PrimeiroTP.java
+++ testedeperformance01/PrimeiroTP.java
@@ -5,7 +5,20 @@ public class PrimeiroTP {
 	private static float[] firstGrade;
 	private static float[] secondGrade;
 
-	private static final int LIMIT = 100; 		
+	private static final int LIMIT = 100; 	
+
+    private static float calculateAverageGrade(int indice){
+		return (firstGrade[indice] + secondGrade[indice]) / 2;
+	}
+	
+	private static String getStudentStatus(float sl){	
+		if(sl < 4) {
+			return "Reprovado";
+		} else if(sl >= 4 || sl < 7) {
+			return "Prova Final";
+		}			
+		return "Aprovado";
+	}	
 
 	public static void main(String[] args) {
 		studentNames = new String[LIMIT];
