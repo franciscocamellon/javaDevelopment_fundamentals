151a181688525084f3207a349aabb59f4ac4ccf8
franciscocamellon <franciscocamellon@gmail.com>
2022-11-03 16:15:16 -0300

    Criar a classe PrimeiroTP

---
+++ testedeperformance01/PrimeiroTP.java
@@ -0,0 +1,16 @@
+
+public class PrimeiroTP {
+
+	private static String[] studentNames;
+	private static float[] firstGrade;
+	private static float[] secondGrade;
+
+	private static final int LIMIT = 100; 		
+
+	public static void main(String[] args) {
+		studentNames = new String[LIMIT];
+		firstGrade = new float[LIMIT];
+		secondGrade = new float[LIMIT];
+		
+	}
+}
