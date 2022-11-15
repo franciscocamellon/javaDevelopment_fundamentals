151286d8e10cccb947dc42dd3b8919a82886b3f9
franciscocamellon <franciscocamellon@gmail.com>
2022-11-15 20:40:01 -0300

    Finished with overloaded methods

--- testedeperformance02/SecondPerformanceTest/src/com/camelloncase/secondtp/tests/SecondPerformanceTest.java
+++ testedeperformance02/SecondPerformanceTest/src/com/camelloncase/secondtp/tests/SecondPerformanceTest.java
@@ -3,27 +3,22 @@ package com.camelloncase.secondtp.tests;
 import java.util.Scanner;
 
 import com.camelloncase.secondtp.auxiliar.Constants;
+import com.camelloncase.secondtp.domain.Person;
 import com.camelloncase.secondtp.domain.Student;
+import com.camelloncase.secondtp.domain.Teacher;
 
 public class SecondPerformanceTest {
 
-	private static Student[] students;
-
-    private static void printReport() {		
-		for (int i = 0; i < Constants.LIMIT; i++) {
-			if(students[i] != null) {
-				students[i].printReport(i);
-			}
-		}
-	}
+	private static Person[] people;
     
-    private static void printReport(int id) {		
-		students[id].printReport(id);
-	}
+    private static void printReport(int id) {
+    	System.out.println("");
+    	people[id].checkState(id);
+    }    
 	
 	public static void main(String[] args) {
 		
-		students = new Student[Constants.LIMIT];
+		people = new Person[Constants.LIMIT];
 
         Scanner in = new Scanner(System.in);
 
@@ -31,20 +26,54 @@ public class SecondPerformanceTest {
 		int indexPosition = 0;
 
         do {
-			System.out.println("[1] Registrar as notas de um novo aluno.");
-			System.out.println("[2] Consultar boletim de um aluno.");
-			System.out.println("[3] Consultar notas da turma.");
-			System.out.println("[4] Sair.");
-			
-			System.out.print("Informe a opção desejada: ");						
+        	
+        	System.out.println("[1] Cadastrar professor.");
+    		System.out.println("[2] Cadastrar aluno.");
+    		System.out.println("[3] Consultar situação de um docente/discente.");
+    		System.out.println("[4] Sair.");
+    		System.out.println("");
+    		System.out.print("Informe a opção desejada: ");
+        	
 			userOption = in.next();
 			
 			switch (userOption) {
+			
 			case "1":
+				
+				Teacher teacher = new Teacher();
+				
+				if(indexPosition < Constants.LIMIT) {
+					
+					System.out.print("Informe o nome do professor: ");	
+					teacher.setName(in.next());
+	
+					System.out.print("Informe a serie: ");	
+					teacher.setClassroom(in.next());
+	
+					System.out.print("Informe a materia lecionada: ");	
+					teacher.setSubject(in.next());
+					
+					System.out.print("Informe se está de férias: ");	
+					teacher.setVacation(in.nextBoolean());
+					
+					people[indexPosition] = teacher;
+					
+					people[indexPosition].checkState(indexPosition);
+
+					indexPosition++;
+
+				} else {
+					
+					System.out.println("Limite de professores cadastrados atingido! Impossível cadastrar um novo aluno.");
+				}
+				break;
+
+			case "2":
 				
 				Student student = new Student();
 				
 				if(indexPosition < Constants.LIMIT) {
+					
 					System.out.print("Informe o nome do aluno: ");	
 					student.setName(in.next());
 	
@@ -54,40 +83,47 @@ public class SecondPerformanceTest {
 					System.out.print("Informe a nota da AV2: ");	
 					student.setSecondGrade(in.nextFloat());
 					
-					student.print();
+					people[indexPosition] = student;
 					
-					students[indexPosition] = student;
+					people[indexPosition].checkState(indexPosition);
 
 					indexPosition++;
 
 				} else {
+					
 					System.out.println("Limite de alunos cadastrados atingido! Impossível cadastrar um novo aluno.");
 				}
 				break;
 
-			case "2":
-				System.out.print("Informe o código do aluno para exibição do boletim: ");						
+			case "3":				
+				
+				System.out.print("Informe o código do docente/discente para exibição da situação: ");						
 				int id = in.nextInt();
 
 				if(id >= 0 && id < indexPosition) {
+					
 					printReport(id);
+					
 				} else {
+					
 					System.out.println("O código ["+id+"] é inválido!!!");
 				}
-				break;
-
-			case "3":
-				printReport();
+				
 				break;
 
 			case "4":
+				
 				System.out.println("Saída");
+				
 				break;
 
 			default:
+				
 				System.out.println("A opção ["+userOption+"] é inválida");
+				
 				break;
 			}
+			
 		} while (!"4".equalsIgnoreCase(userOption));
 				
 		System.out.println("Processamento finalizado!!!");
