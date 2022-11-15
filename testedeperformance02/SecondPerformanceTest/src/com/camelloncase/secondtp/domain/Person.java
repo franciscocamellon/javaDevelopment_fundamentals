52489224e54b71d54717ed2688b8775100fc9b91
franciscocamellon <franciscocamellon@gmail.com>
2022-11-15 20:37:36 -0300

    Add checkStatus method

---
+++ testedeperformance02/SecondPerformanceTest/src/com/camelloncase/secondtp/domain/Person.java
@@ -0,0 +1,39 @@
+package com.camelloncase.secondtp.domain;
+
+import com.camelloncase.secondtp.auxiliar.Constants;
+
+public class Person {
+	
+	private String name;
+	
+	public Person() {
+		this.name = Constants.DEFAULT_NAME;
+	}
+	
+	public Person(String name) {
+		this();
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return this.name;
+	}
+	
+	public void checkState() {
+		System.out.println(toString());
+	}
+	
+	public void checkState(int id) {
+		System.out.println("<" + id + "> " + toString());
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+}
