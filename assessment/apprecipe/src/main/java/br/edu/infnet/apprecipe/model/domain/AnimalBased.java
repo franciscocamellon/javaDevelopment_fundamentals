79169e99ee8a012b31bbfcc42cd56a1e50ea57dc
franciscocamellon <franciscocamellon@gmail.com>
2022-12-19 23:02:05 -0300

    Add attributes and methods to AnimalBased child class and create a test class

--- assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/domain/AnimalBased.java
+++ assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/domain/AnimalBased.java
@@ -2,4 +2,59 @@ package br.edu.infnet.apprecipe.model.domain;
 
 public class AnimalBased extends Ingredient {
 
+	private String origin;
+	private String meatForm;
+	private boolean exoticMeat;
+	
+	public AnimalBased(String name, double weight, double cost, boolean exoticMeat) {
+		super(name, weight, cost);
+		this.exoticMeat = exoticMeat;
+	}
+
+	@Override
+	public double costCalculator() {
+		if (exoticMeat) {
+			return this.getCost() * this.getWeight() + 10;
+		}
+		return this.getCost() * this.getWeight();
+	}
+	
+	@Override
+	public String toString() {
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(super.toString());
+		sb.append(";");
+		sb.append(origin);
+		sb.append(";");
+		sb.append(meatForm);
+		sb.append(";");
+		sb.append(exoticMeat ? "Proteína exótica" : "Proteína normal");
+		
+		return sb.toString();
+	}
+	
+	public String getOrigin() {
+		return origin;
+	}
+	
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+	
+	public String getMeatForm() {
+		return meatForm;
+	}
+	
+	public void setMeatForm(String meatForm) {
+		this.meatForm = meatForm;
+	}
+
+	public boolean isExoticMeat() {
+		return exoticMeat;
+	}
+
+	public void setExoticMeat(boolean exoticMeat) {
+		this.exoticMeat = exoticMeat;
+	}
 }
--- assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/tests/AnimalBasedTest.java
+++ assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/tests/AnimalBasedTest.java
@@ -6,11 +6,23 @@ public class AnimalBasedTest {
 
 	public static void main(String[] args) {
 		
-		AnimalBased ab1 = new AnimalBased();
+		AnimalBased ab1 = new AnimalBased("Salmão", 2, 20, false);
+		ab1.setOrigin("Pesca");
+		ab1.setMeatForm("Fresco");
+		ab1.toString();
+		System.out.println(ab1);
 		
-		AnimalBased ab2 = new AnimalBased();
+		AnimalBased ab2 = new AnimalBased("Javali", 2, 20, true);
+		ab2.setOrigin("Caça");
+		ab2.setMeatForm("Congelado");
+		ab2.toString();
+		System.out.println(ab2);
 		
-		AnimalBased ab3 = new AnimalBased();
+		AnimalBased ab3 = new AnimalBased("Wagyu", 1, 200, false);
+		ab3.setOrigin("Manejo");
+		ab3.setMeatForm("Refrigerado");
+		ab3.toString();
+		System.out.println(ab3);
 		
 	}
 }
