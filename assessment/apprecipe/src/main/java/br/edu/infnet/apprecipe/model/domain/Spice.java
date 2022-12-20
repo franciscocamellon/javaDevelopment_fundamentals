e90055da1dafe01fd3cca366bf7e0f9c45d1a2ee
franciscocamellon <franciscocamellon@gmail.com>
2022-12-19 23:03:03 -0300

    Add attributes and methods to Spice child class and create a test class

--- assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/domain/Spice.java
+++ assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/domain/Spice.java
@@ -1,5 +1,58 @@
 package br.edu.infnet.apprecipe.model.domain;
 
 public class Spice extends Ingredient {
+	
+	private String spiceForm;
+	private String taste;
+	private String color;
+
+	public Spice(String name, double weight, double cost, String color) {
+		super(name, weight, cost);
+		this.color = color;
+	}
+
+	@Override
+	public double costCalculator() {
+		return this.getWeight() * this.getCost();
+	}
+	
+	@Override
+	public String toString() {
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(super.toString());
+		sb.append(";");
+		sb.append(spiceForm);
+		sb.append(";");
+		sb.append(taste);
+		sb.append(";");
+		sb.append(color);
+		
+		return sb.toString();
+	}
+
+	public String getSpiceForm() {
+		return spiceForm;
+	}
+
+	public void setSpiceForm(String spiceForm) {
+		this.spiceForm = spiceForm;
+	}
+
+	public String getTaste() {
+		return taste;
+	}
+
+	public void setTaste(String taste) {
+		this.taste = taste;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
 
 }
--- assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/tests/SpiceTest.java
+++ assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/tests/SpiceTest.java
@@ -6,11 +6,20 @@ public class SpiceTest {
 
 	public static void main(String[] args) {
 
-		Spice sp1 = new Spice();
+		Spice sp1 = new Spice("Páprica", 0.1, 1, "Avermelhado");
+		sp1.setSpiceForm("Moído");
+		sp1.setTaste("Agridoce");
+		System.out.println(sp1);
 		
-		Spice sp2 = new Spice();
+		Spice sp2 = new Spice("Pimenta do reino", 0.15, 0.5, "Incolor");
+		sp2.setSpiceForm("Moído");
+		sp2.setTaste("Apimentado");
+		System.out.println(sp2);
 		
-		Spice sp3 = new Spice();
+		Spice sp3 = new Spice("Aniz estrelado", 0.20, 1, "Incolor");
+		sp3.setSpiceForm("Fresco");
+		sp3.setTaste("Azedo");
+		System.out.println(sp3);
 
 	}
 
