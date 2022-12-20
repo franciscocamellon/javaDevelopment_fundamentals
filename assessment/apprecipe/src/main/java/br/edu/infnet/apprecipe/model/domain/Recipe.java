10ef073fb021a09545c2c00b339d5b39d215c1cf
franciscocamellon <franciscocamellon@gmail.com>
2022-12-19 23:04:51 -0300

    Create and set the principal class Recipe

--- assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/domain/Recipe.java
+++ assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/domain/Recipe.java
@@ -1,5 +1,62 @@
 package br.edu.infnet.apprecipe.model.domain;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 public class Recipe {
+	
+	private String name;
+	private int quantity;
+	private LocalDateTime requestDate;
+	private Chef chef;
+	private List<Ingredient> ingredients;
+	
+	public Recipe() {
+		requestDate = LocalDateTime.now();
+	}
+	
+	@Override
+	public String toString() {
+
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		return String.format("%s,%s,%d,%s", name, chef, quantity, requestDate.format(format));
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Chef getChef() {
+		return chef;
+	}
+
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+
+	public List<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(List<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
+
+	public LocalDateTime getRequestDate() {
+		return requestDate;
+	}
 
 }
--- assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/tests/RecipeTest.java
+++ assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/tests/RecipeTest.java
@@ -1,17 +1,35 @@
 package br.edu.infnet.apprecipe.model.tests;
 
+import br.edu.infnet.apprecipe.model.domain.Chef;
 import br.edu.infnet.apprecipe.model.domain.Recipe;
 
 public class RecipeTest {
 
 	public static void main(String[] args) {
 		
+		Chef c1 = new Chef("Joao", "Almeria", "joao.almeria@email.com" );
+		
 		Recipe r1 = new Recipe();
+		r1.setName("Primeira receita");
+		r1.setQuantity(3);
+		r1.setChef(c1);
+		//r1.setIngredients(ingredients);
+		System.out.println(r1);
 		
 		Recipe r2 = new Recipe();
+		r2.setName("Primeira receita");
+		r2.setQuantity(4);
+		r2.setChef(c1);
+		//r2.setIngredients(ingredients);
+		System.out.println(r2);
 		
 		Recipe r3 = new Recipe();
-
+		r3.setName("Primeira receita");
+		r3.setQuantity(1);
+		r3.setChef(c1);
+		//r3.setIngredients(ingredients);
+		System.out.println(r3);
+		
 	}
 
 }
