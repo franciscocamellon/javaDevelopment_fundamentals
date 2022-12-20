3fee6c528961409c3259d828403858af3eab4dcc
franciscocamellon <franciscocamellon@gmail.com>
2022-12-19 23:02:35 -0300

    Add attributes and methods to PlantBased child class and create a test class

--- assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/domain/PlantBased.java
+++ assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/domain/PlantBased.java
@@ -1,5 +1,61 @@
 package br.edu.infnet.apprecipe.model.domain;
 
 public class PlantBased extends Ingredient {
+	
+	private boolean organic;
+	private String form;
+	private String unit;
+
+	public PlantBased(String name, double weight, double cost, boolean organic) {
+		super(name, weight, cost);
+		this.organic = organic;
+	}
+
+	@Override
+	public double costCalculator() {
+		if (organic) {
+			return this.getCost() * this.getWeight() + 20;
+		}
+		return this.getCost() * this.getWeight();
+	}
+	
+	@Override
+	public String toString() {
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(super.toString());
+		sb.append(";");
+		sb.append(organic ? "Vegetal orgânico" : "Vegetal normal");
+		sb.append(";");
+		sb.append(form);
+		sb.append(";");
+		sb.append(unit);
+		
+		return sb.toString();
+	}
+
+	public boolean isOrganic() {
+		return organic;
+	}
+
+	public void setOrganic(boolean organic) {
+		this.organic = organic;
+	}
+
+	public String getForm() {
+		return form;
+	}
+
+	public void setForm(String form) {
+		this.form = form;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
 
 }
--- assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/tests/PlantBasedTest.java
+++ assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/tests/PlantBasedTest.java
@@ -6,11 +6,20 @@ public class PlantBasedTest {
 
 	public static void main(String[] args) {
 
-		PlantBased pb1 = new PlantBased();
+		PlantBased pb1 = new PlantBased("Alho poró", 0.2, 20, false);
+		pb1.setForm("Cru");
+		pb1.setUnit("Maço");
+		System.out.println(pb1);
 		
-		PlantBased pb2 = new PlantBased();
+		PlantBased pb2 = new PlantBased("Brócolis", 1, 15, true);
+		pb2.setForm("Cru");
+		pb2.setUnit("Maço");
+		System.out.println(pb2);
 		
-		PlantBased pb3 = new PlantBased();
+		PlantBased pb3 = new PlantBased("Batata", 1, 10, false);
+		pb3.setForm("Pré-cozido");
+		pb3.setUnit("Kilo");
+		System.out.println(pb3);
 
 	}
 
