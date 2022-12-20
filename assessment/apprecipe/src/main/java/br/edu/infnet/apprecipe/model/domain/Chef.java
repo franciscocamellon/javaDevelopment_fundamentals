03f79bccb38db1d15d9dd22ec70e4a940ae42a06
franciscocamellon <franciscocamellon@gmail.com>
2022-12-19 23:04:25 -0300

    Create and set the association class Chef

--- assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/domain/Chef.java
+++ assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/domain/Chef.java
@@ -1,5 +1,39 @@
 package br.edu.infnet.apprecipe.model.domain;
 
 public class Chef {
+	
+	public String name;
+	public String restaurant;
+	public String email;
+	
+	
+	public Chef(String name, String restaurant, String email) {
+		this.name = name;
+		this.restaurant = restaurant;
+		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(name);
+		sb.append(";");
+		sb.append(restaurant);
+		sb.append(";");
+		sb.append(email);
+		
+		return sb.toString();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public String getRestaurant() {
+		return restaurant;
+	}
+	public String getEmail() {
+		return email;
+	}
 
 }
--- assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/tests/ChefTest.java
+++ assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/tests/ChefTest.java
@@ -6,11 +6,14 @@ public class ChefTest {
 
 	public static void main(String[] args) {
 
-		Chef c1 = new Chef();
+		Chef c1 = new Chef("Joao", "Almeria", "joao.almeria@email.com" );
+		System.out.println(c1);
 		
-		Chef c2 = new Chef();
+		Chef c2 = new Chef("Joao", "Almeria", "joao.almeria@email.com" );
+		System.out.println(c2);
 		
-		Chef c3 = new Chef();
+		Chef c3 = new Chef("Joao", "Almeria", "joao.almeria@email.com" );
+		System.out.println(c3);
 
 	}
 
