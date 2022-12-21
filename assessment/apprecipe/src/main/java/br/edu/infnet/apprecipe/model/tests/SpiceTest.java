85ccc861fd0afd165d8cfc5d16a3c49e69eb7d7d
franciscocamellon <franciscocamellon@gmail.com>
2022-12-20 22:11:37 -0300

    Handling exceptions

--- assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/tests/SpiceTest.java
+++ assessment/apprecipe/src/main/java/br/edu/infnet/apprecipe/model/tests/SpiceTest.java
@@ -1,26 +1,37 @@
 package br.edu.infnet.apprecipe.model.tests;
 
 import br.edu.infnet.apprecipe.model.domain.Spice;
+import br.edu.infnet.apprecipe.model.exceptions.ZeroCostException;
 
 public class SpiceTest {
 
 	public static void main(String[] args) {
 
+		try {
 		Spice sp1 = new Spice("Páprica", 0.1f, 1, "Avermelhado");
 		sp1.setSpiceForm("Moído");
 		sp1.setTaste("Agridoce");
 		System.out.println(sp1);
+		} catch (ZeroCostException e) {
+			System.out.println("[ERRO] " + e.getMessage());
+		}
 		
-		Spice sp2 = new Spice("Pimenta do reino", 0.15f, 0.5f, "Incolor");
+		try {
+		Spice sp2 = new Spice("Pimenta do reino", 0.15f, -0.5f, "Incolor");
 		sp2.setSpiceForm("Moído");
 		sp2.setTaste("Apimentado");
 		System.out.println(sp2);
+		} catch (ZeroCostException e) {
+			System.out.println("[ERRO] " + e.getMessage());
+		}
 		
-		Spice sp3 = new Spice("Aniz estrelado", 0.20f, 1, "Incolor");
+		try {
+		Spice sp3 = new Spice("Aniz estrelado", 0.20f, 0, "Incolor");
 		sp3.setSpiceForm("Fresco");
 		sp3.setTaste("Azedo");
 		System.out.println(sp3);
-
+		} catch (ZeroCostException e) {
+			System.out.println("[ERRO] " + e.getMessage());
+		}
 	}
-
 }
